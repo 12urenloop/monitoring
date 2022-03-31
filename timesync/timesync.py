294d5076c624ac64314f6b4a4bc4eb74080adebc
redfast00 <redfast00@gmail.com>
2022-03-31 18:14:48 +0200

    Baton status

--- timesync/timesync.py
+++ timesync/timesync.py
@@ -1,9 +1,16 @@
+from socket import timeout
 from flask import Flask, jsonify, Blueprint
 import requests
 import time
 from threading import Thread
-from config import SERVERS, REFRESH_INTERVAL
+from dataclasses import dataclass
+from config import REFRESH_INTERVAL, TELRAAM_STATION_URL, EXTRA_SERVERS, LOGGING_LEVEL
+import traceback
+import datetime
+import logging
 
+logging.getLogger().setLevel(LOGGING_LEVEL)
+server_start_moment = time.time()
 
 class TimeMeasurement:
     def __init__(self, before, server_time, after):
@@ -20,33 +27,146 @@ class TimeMeasurement:
         return self.after - self.before
 
 
-def get_server_sync(url):
+def get_server_sync(name, url):
     before = time.time()
     try:
-        r = requests.get(url, timeout=1)
+        r = requests.get(url + '/time', timeout=1)
         server_time = r.json()['timestamp'] / 1000
-    except:
+        logging.info(f"Fetched timesync {name}")
+    except requests.exceptions.Timeout:
+        logging.warning(f"Failed timesync {name}")
+        return None
+    except Exception as e:
+        logging.error(f"Failed timesync {name}")
+        logging.error(traceback.format_exc())
         return None
     after = time.time()
     return TimeMeasurement(before, server_time, after)
 
-def serialize(name, measurement):
+def serialize_timemeasurement(name, measurement):
         if measurement is None:
-            return {'name': name, 'success': False, 'offset': 0, 'accuracy': 0}
+            r =  {'name': name, 'success': False, 'offset': 0, 'accuracy': 0}
         else:
-            return {'name': name, 'success': True, 'offset': measurement.offset(), 'accuracy': measurement.accuracy()}
+            r =  {'name': name, 'success': True, 'offset': measurement.offset(), 'accuracy': measurement.accuracy()}
+        r['abs_offset'] = abs(r['offset'])
+        r['abs_accuracy'] = abs(r['accuracy'])
+        return r
 
+@dataclass
+class BatonStatus:
+    last_seen: float = 0
+    battery: float = 0
+    uptime: float = 0
+    rebooted: bool = False
+
+
+def serialize_batonstatus(mac, baton_status: BatonStatus, name: str):
+    return {
+        'mac': mac,
+        'battery': baton_status.battery,
+        'uptime': str(datetime.timedelta(seconds=baton_status.uptime / 1000)),
+        'rebooted': baton_status.rebooted,
+        'time_since_seen': time.time() - baton_status.last_seen,
+        'name': name
+    }
 
 server_status = {}
+station_urls = {}
+baton_status_dict = {}
+per_station_last_id = {}
+baton_name_to_mac = {}
+baton_mac_to_name = {}
+
+def first_initialize_stations():
+    for name, url in station_urls.items():
+        try:
+            d = requests.get(url + '/last_detection', timeout=1).json()
+            per_station_last_id[name] = d['detection']['id']
+            logging.info(f"First init {name} now at {per_station_last_id[name]}")
+        except requests.exceptions.Timeout:
+            logging.info(f"Failed first init {name} (timeout)")
+        except:
+            logging.error(traceback.format_exc())
+            logging.error(f"Failed first init {name}")
+
+        
+
+def update_from_station(station_name, url):
+    try:
+        detections = requests.get(url + f'/detections/{per_station_last_id.get(station_name, 0)}', timeout=1).json()['detections']
+        begin = per_station_last_id.get(station_name, 0)
+        for detection in detections:
+            mac = detection['mac']
+            uptime = detection['uptime_ms']
+            detection_time = detection['detection_timestamp']
+            battery = detection['battery']
+            if mac not in baton_status_dict:
+                baton_status_dict[mac] = BatonStatus(0)
+            baton_status = baton_status_dict[mac]
+            
+            if baton_status.last_seen < detection_time and detection_time > server_start_moment:
+                if uptime < baton_status.uptime - 3000:
+                    baton_status.rebooted = True
+                baton_status.last_seen = detection_time
+                baton_status.battery = battery
+                baton_status.uptime = uptime
+            per_station_last_id[station_name] = max(detection['id'], per_station_last_id.get(station_name, 0))
+        logging.info(f"Success station update {station_name} got {per_station_last_id.get(station_name, 0) - begin} records, now at {per_station_last_id[station_name]}")
+    except requests.exceptions.Timeout:
+        logging.warning(f"Failed station update {station_name} (timeout)")
+    except:
+        logging.error(f"Failed station update {station_name}")
+        logging.error(traceback.format_exc())
+
 
 def fetch_routine():
-    print("Starting fetch routine")
-    print(f"Will refresh a timestamps every {REFRESH_INTERVAL} s")
+    first_initialized = False
     while True:
-        for name, url in SERVERS.items():
-            server_status[name] = get_server_sync(url)
-            print(f"Fetched {name}")
-            time.sleep(REFRESH_INTERVAL / len(SERVERS))
+        logging.info(f"Will refresh every {REFRESH_INTERVAL} s")
+        try:
+            data = requests.get(TELRAAM_STATION_URL + '/station', timeout=1).json()
+            if 'name' in data[0]:
+                station_urls.clear()
+                for station_obj in data:
+                    station_urls[station_obj['name']] = station_obj['url']
+                logging.info("Telraam fetch stations success")
+        except:
+            logging.error("Telraam fetch stations failed")
+        
+        try:
+            data = requests.get(TELRAAM_STATION_URL + '/baton', timeout=1).json()
+            if 'name' in data[0]:
+                baton_mac_to_name.clear()
+                baton_name_to_mac.clear()
+                for baton_obj in data:
+                    mac = baton_obj['mac'].lower()
+                    name = baton_obj['name']
+                    baton_mac_to_name[mac] = name
+                    baton_name_to_mac[name] = mac
+                    if mac not in baton_status_dict:
+                        baton_status_dict[mac] = BatonStatus()
+                logging.info("Telraam fetch batons success")
+        except:
+            logging.error("Telraam fetch batons failed")
+
+        # # FETCH TIMESYNC
+        logging.info("Starting timestamp fetch routine")
+        for name, url in station_urls.items() :
+            server_status[name] = get_server_sync(name, url)
+        for name, url in EXTRA_SERVERS.items() :
+            server_status[name] = get_server_sync(name, url)
+        
+
+
+        # FETCH BATON STATUS
+        if not first_initialized:
+            first_initialize_stations()
+            first_initialized = True
+
+        for name, url in station_urls.items() :
+            update_from_station(name, url)
+            logging.info(f"Fetched stations {name}")
+        time.sleep(REFRESH_INTERVAL)
 
 
 root = Blueprint('root', __name__, url_prefix='')
@@ -54,10 +174,18 @@ root = Blueprint('root', __name__, url_prefix='')
 @root.route('/')
 def home():
     return jsonify([
-        serialize(name, measurement)
+        serialize_timemeasurement(name, measurement)
         for name, measurement
         in server_status.items()
     ])
+
+@root.route('/batons')
+def batons():
+    return jsonify(sorted([
+        serialize_batonstatus(mac, batonstatus, baton_mac_to_name[mac])
+        for mac, batonstatus
+        in baton_status_dict.items()
+    ], key=lambda x: x['name']))
 
 
 def create_app():
